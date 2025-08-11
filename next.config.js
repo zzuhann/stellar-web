c7bb054f9268f526a513632a1e8741d66ce59186
zzuhann <zzuhanlin@gmail.com>
2025-08-11 21:55:22 +0800

    feat: 在 next.config.js 中新增 CDN 域名 cdn.stellar-zone.com 以支持圖片上傳功能

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
     domains: [
       'pub-b7b01bb9cbef44f2bdd3b7b3a5c1b4b7.r2.dev',
       'pub-1ea260dddf7f40e4b473626d08cc1689.r2.dev',
+      'cdn.stellar-zone.com',
     ], // R2
     minimumCacheTTL: 1 * 60 * 60 * 24, // 24 hours
   },
