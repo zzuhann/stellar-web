ec22ff5fd94457de8eabe37dd14d8246d9d74ce2
zzuhann <zzuhanlin@gmail.com>
2026-02-07 10:23:29 +0800

    fix: adjust margin

--- src/components/HomePage/components/EventsTab.tsx
+++ src/components/HomePage/components/EventsTab.tsx
@@ -8,6 +8,7 @@ const eventListContainer = css({
   display: 'grid',
   gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
   gap: '16px',
+  marginTop: '16px',
   '@media (min-width: 600px)': {
     gridTemplateColumns: 'repeat(2, 1fr)',
   },
