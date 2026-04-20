f7aab4f230357f4c090359a50c688c467c4c4071
zzuhann <zzuhanlin@gmail.com>
2026-04-21 03:54:33 +0800

    chore: update dependency

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for STELLAR PWA
-const VERSION = '20260304T16095';
+const VERSION = '20260420T19503';
 const CACHE_NAME = `stellar-cache-v${VERSION}`;
 const STATIC_CACHE_URLS = [
   '/',
