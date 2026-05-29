94be9dd326ce1e1246bb08ce1f4b73ac1646847b
zzuhann <zzuhanlin@gmail.com>
2026-05-30 07:25:19 +0800

    chore: only enabled when in production

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -6,9 +6,10 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled: process.env.NODE_ENV === 'production',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1,
+  tracesSampleRate: 0.1,
   // Enable logs to be sent to Sentry
   enableLogs: true,
 });
