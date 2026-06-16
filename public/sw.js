7a40f1a7c790e6ef3a031f9ab43c0f1e947c541f
zzuhann <zzuhanlin@gmail.com>
2026-06-17 00:05:19 +0800

    chore: adjust sw

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for STELLAR PWA
-const VERSION = '20260609T07380';
+const VERSION = '20260616T16005';
 const CACHE_NAME = `stellar-cache-v${VERSION}`;
 const STATIC_CACHE_URLS = [
   '/',
